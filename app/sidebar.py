6f7073a36b023ebfcaf4def1bc71c6825c2335c7
Robert Rita <robertrita@microsoft.com>
2024-08-01 10:53:34 +0800

    updated sidebar

--- app/sidebar.py
+++ app/sidebar.py
@@ -5,11 +5,11 @@ import streamlit as st
 def show_sidebar():
     st.subheader("💡Info", anchor=False)
     st.write(
-        "This is a Streamlit app for **Q&A Evaluator with RAG** with Nodejs backend api powered by Azure OpenAI."
+        "This is a Streamlit app for **Q&A Evaluator with RAG** in Nodejs backend api powered by Azure OpenAI."
     )
 
     st.subheader("🛠Technology Stack", anchor=False)
-    st.write("Python, Streamlit, Azure OpenAI, Azure AI")
+    st.write("Python, Streamlit, Azure OpenAI, Azure AI Search")
     st.write(
         "Check out the repo here: [Q&A RAG Nodejs Backend](https://github.com/robrita/Azure-Q-A-RAG-Nodejs-Backend)"
     )
